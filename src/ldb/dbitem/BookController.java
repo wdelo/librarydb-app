4e188da8e13c0e3f081a54251e0701e74f370194
William Delo <55678535+wdelo@users.noreply.github.com>
2022-04-06 01:19:45 -0400

    Added DBItemController interface and all relevant implementers
    Also packaged them into ldb.dbitem

---
+++ src/ldb/dbitem/BookController.java
@@ -0,0 +1,38 @@
+package ldb.dbitem;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class BookController implements DBItemController {
+
+	@Override
+	public void insert(Connection conn, Scanner in) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void edit(Connection conn, String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Connection conn, String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void search(Connection conn, Scanner in) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void retrieve(Connection conn, Scanner in) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
