895513ba7913a6ad90c161cb4a2b39cb2b9a41b2
C D-00-Dawg <C D-00-Dawg@DESKTOP-OS746PM>
2022-04-14 02:21:27 -0400

    Implemented Review controller

--- src/ldb/dbitem/ReviewController.java
+++ src/ldb/dbitem/ReviewController.java
@@ -5,14 +5,32 @@ import java.util.Scanner;
 
 import ldb.UserOption;
 import ldb.util.DBUtils;
+import ldb.util.MenuScreen;
 
 public class ReviewController {
 
+	private static String conditionMenuPrompt = "What would you like to manage with this condition log?";	
+	private static String[] conditionMenuScreenOptions = {
+    		"Add a Review", 
+    		"Select a Review",
+    		"Back",
+	};
+	
+	private static String selectedMenuPrompt = "What would you like to do with this Review?";
+	private static String[] selectedMenuScreenOptions = {
+			"Delete this Review",
+			"Edit this Review",
+			"Back",
+	};
+	
+	private static MenuScreen menuScreen = new MenuScreen(conditionMenuPrompt, conditionMenuScreenOptions);
+	private static MenuScreen selectedMenuScreen = new MenuScreen(selectedMenuPrompt, selectedMenuScreenOptions);
+	
 	public static String[] insert(Connection conn, Scanner in, String[] parentIds) {
 		System.out.println("Let's find the patron who is submitting this review. Please enter their last name:");
 		String lname = in.nextLine();
 		
-		String sql = "SELECT Email_Address, Fname, Lname FROM Patron WHERE Lname = '"+lname+"';";
+		String sql = "SELECT Email, Fname, Lname FROM Patron WHERE Lname = '"+lname+"';";
 		String[] email = DBUtils.searchAndSelect(conn, in, sql, 3, "Email_Address");
 		
 		System.out.println("Let's find the piece of media this patron is reviewing. Please enter its title:");
@@ -36,8 +54,11 @@ public class ReviewController {
 	}
 
 	public static void edit(Connection conn, Scanner in, String[] ids) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Please enter the desired rating (1-10):");
+		int rating = DBUtils.getValidInput(1, 10, in);
+		System.out.println("Please enter the desired response:");
+		String content = in.nextLine();
+		DBUtils.editRecord(conn, "Review", 2, "PatronEmail", "'"+ids[0]+"'", "MediaID", "'"+ids[1]+"'", "Rating", "'"+rating+"'", "Response", "'"+content+"'");
 	}
 
 	public static void delete(Connection conn, Scanner in, String[] ids) {
@@ -73,12 +94,41 @@ public class ReviewController {
 	}
 
 	public static void execute(Connection conn, Scanner in, String[] parentIds) {
-		// TODO Auto-generated method stub
+		menuScreen.displayBlank();
+		int menuSelection = menuScreen.getOption(in);
+		switch (menuSelection) {
+		case 1:
+			insert(conn, in, parentIds);
+			break;
+		case 2:
+			view(conn, in, parentIds);
+			break;
+		default:
+			break;
+		}
 		
 	}
 	
 	public static void view(Connection conn, Scanner in, String[] parentIds) {
-		
+		String[] ids = retrieve(conn, in, parentIds);
+		if (ids != null) {
+			selectedMenuScreen.display();
+			int menuSelection = selectedMenuScreen.getOption(in);
+			switch (menuSelection) {
+			case 1:			
+				delete(conn, in, ids);
+				break;
+			case 2:
+				edit(conn, in, ids);
+				break;
+			default:
+				break;
+			}	
+		} else {
+			DBUtils.blank();
+			System.out.println("No reviews :(\nType \"1\" to continue to the main menu.");
+			DBUtils.getValidInput(1, 1, in);
+		}
 	}
 
 }
